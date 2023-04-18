ae328d02ff889c63c45a87e2d8487dd51d492d8a
MeshachArinze <meshacharinze@gmail.com>
2023-04-18 13:06:45 +0100

    make new changes to repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // in src/App.tsx
-import { Admin } from "react-admin";
-
+import React from "react";
+import { Admin, Resource } from "react-admin";
+import { UserList } from "./components/Users";
 import jsonServerProvider from "ra-data-json-server";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
  <Admin dataProvider={dataProvider}>
-   <Resource name="users" list={ListGuesser} />
+   <Resource name="users" list={UserList}  />
  </Admin>
 )
 export default App;
