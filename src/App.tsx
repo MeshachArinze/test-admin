063726210be3023e8c46578f6f311eac95e4bf24
MeshachArinze <meshacharinze@gmail.com>
2023-04-18 12:45:14 +0100

    commit to main

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import jsonServerProvider from "ra-data-json-server";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
-const App = () => <Admin dataProvider={dataProvider} />;
-
+const App = () => (
+ <Admin dataProvider={dataProvider}>
+   <Resource name="users" list={ListGuesser} />
+ </Admin>
+)
 export default App;
